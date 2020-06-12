bde3dabad4cc1de0aab69ac3e865e9df36a1241f
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-12 23:10:35 +0500

    javascript assignment chapter1

---
+++ Chapter1/app.js
@@ -0,0 +1,16 @@
+// CHAPTER 1
+// Task 1
+// alert("Javascript Alerts");
+
+// Task 2
+// alert("Javascript Alerts \r\n Error! Please Enter a valid password");
+
+// Task 3
+// alert("Javascript Alerts \r\n \r\n Welcome to JS land.. \r\n HAppy coding");
+
+// Task 4
+// alert("Javascript Alerts \r\n \r\n Welcome to JS land..");
+// alert("Javascript Alerts \r\n \r\n Happy Coding");
+
+// Task 5
+// alert("Javascript Alerts \r\n \r\n Hello I can run JS through my web browser's console");
