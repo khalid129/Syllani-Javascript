79b82d8c4db7fed8fb2c94aff7549f0cd9a7d1f3
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-12 23:16:47 +0500

    javascript assignment chapter2

---
+++ Chapter2/app.js
@@ -0,0 +1,38 @@
+// CHAPTER2
+// Task 1
+// var username;
+
+// Task 2
+// var myname="Muhammad Khalid";
+
+// Task 3
+// var message;
+// message = "Hello world";
+// alert(message);
+
+// Task 4
+// var myname="Muhammad Khalid";
+// var age=20;
+// var field="Web & Mobile app Development";
+// alert(myname);
+// alert(age + " years old");
+// alert(field);
+
+// Task 5
+// var name = "PIZZA";
+// alert(name.slice(0,5) + "\r\n" + name.slice(0,4) + "\r\n" + name.slice(0,3) + "\r\n" + name.slice(0,2) + "\r\n" + name.slice(0,1))
+
+// Task 6
+// var email="muhammadkhalid.ch129@gmail.com";
+// alert("My email address is " + email);
+
+// Task 7
+// var book="Smarter way to learn Javascript";
+// alert("I am trying to learn from a book " + book);
+
+// Task 8
+// document.write("yah! I can write HTML content through Javascript");
+
+// Task 9
+// var pattern = " “▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬” ";
+// alert(pattern);
