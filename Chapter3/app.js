9a5d84f4ec9f2898c9b54bfc7244306956e86932
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-13 23:52:25 +0500

    javascript assignment chapter3

---
+++ Chapter3/app.js
@@ -0,0 +1,16 @@
+// CHAPTER 3
+// Task 1
+// var age = 20;
+// alert("I am " + age +" years old");
+
+// Task 2
+
+// Task 3
+// var birth = 2000;
+// document.write("My date of birth is "+ birth + "<br>" + "Date type of my decleared variable is number" +"<br>");
+
+// Task 4
+// var name = prompt("Enter Your name"); 
+// var product = prompt("Enter the product you purchase");
+// var quantity = prompt("Enter the product quantity");
+// document.write(name + " purchased " + quantity + " " + product +" from xyz clothes stores");
