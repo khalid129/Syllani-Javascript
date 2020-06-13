20936b85f4cf892f0d98d47aa88cb19ce6fa11cc
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-14 00:16:14 +0500

    javascript assignment chapter4

---
+++ Chapter4/app.js
@@ -0,0 +1,14 @@
+// CHAPTER 4
+// Task 1
+// var x,y,z;
+
+// Task 2
+// var name,age,section,gender,number;
+// var class , 1 ,-name,@name, my name;
+
+// Task 3
+// document.write("<b>Rules for naming JS variables</b>"+"<br>"+"<br>");
+// document.write("Variable name can only contain number, $ and _ , For example: $My_1stVariable"+"<br>");
+// document.write("Variable must begins with Letter, $, _ . For example: name, $name, _name"+"<br>");
+// document.write("variable names are case sensetive"+"<br>");
+// document.write("Variable name should not be JS keyword");
