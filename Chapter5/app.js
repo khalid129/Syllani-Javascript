802c9635a3ff3c7b533184cf7e13868fd2845fb5
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-16 18:57:17 +0500

    javascript assignment chapter5

---
+++ Chapter5/app.js
@@ -0,0 +1,135 @@
+// CHAPTER 5
+// Task 1
+// document.write("<b>Addition</b>" + "<br>")
+// var num1=prompt("Enter 1st number for Addition");
+// var a = parseInt(num1);
+// var num2=prompt("Enter 2nd number for Addition");
+// var b = parseInt(num2);
+// var sum = a+b;
+// document.write("The Addition of " + num1 + " and " + num2 + " is " + sum + "<br>");
+
+// Task 2
+// document.write("<b>Subtraction</b>" + "<br>")
+// var num1=prompt("Enter 1st number for Subtraction");
+// var a = parseInt(num1);
+// var num2=prompt("Enter 2nd number for Subtraction");
+// var b = parseInt(num2);
+// var sum = a-b;
+// document.write("The Subtraction of " + num1 + " and " + num2 + " is " + sum + "<br>"+ "<br>");
+
+// document.write("<b>Multiplication</b>" + "<br>")
+// var num1=prompt("Enter 1st number for Multiplication");
+// var a = parseInt(num1);
+// var num2=prompt("Enter 2nd number for Multiplication");
+// var b = parseInt(num2);
+// var sum = a*b;
+// document.write("The Multiplication of " + num1 + " and " + num2 + " is " + sum + "<br>"+ "<br>");
+
+// document.write("<b>Division</b>" + "<br>")
+// var num1=prompt("Enter 1st number for Division");
+// var a = parseFloat(num1);
+// var num2=prompt("Enter 2nd number for Division");
+// var b = parseFloat(num2);
+// var sum = a/b;
+// document.write("The Division of " + num1 + " and " + num2 + " is " + sum + "<br>"+ "<br>");
+
+// Task 3
+// var number;
+// document.write("Value after variable decleration is "+ number + "<br>");
+// number = 5;
+// document.write("The initial value is "+ number + "<br>");
+// ++number;
+// document.write("The value after increment is "+ number + "<br>");
+// number=number+7;
+// document.write("The value after addition is "+ number + "<br>");
+// --number;
+// document.write("The value after decrement is "+ number + "<br>");
+// var remainder= number%3;
+// document.write("The remainder value is "+ remainder + "<br>");
+
+// Task 4
+// var ticket = 600;
+// document.write("Price of the 1 ticket is " + ticket + " PKR" + "<br>")
+// document.write("The price of the 5 ticket is " + 5*ticket + " PKR" + "<br>");
+
+// Task 5
+// document.write("Table of 5" + "<br>");
+// for(var i=1; i<=10; i++){
+//     document.write("5"+"x"+i+"="+5*i+" <br>");
+// }
+
+// Task 6
+// var c1 =prompt("Enter a temperature in celcius");
+// var celcius=parseFloat(c1);
+// var f1 =prompt("Enter a temperature in Farenheit");
+// var farenheit=parseFloat(f1);
+// var c2=(farenheit-32)*5/9;
+// var f2=(celcius*9/5)+32;
+// document.write(celcius + " celcius is " +f2 +" farenheit" +"<br>");
+// document.write(farenheit + " farenheit is " +c2 +" celcius" +"<br>");
+
+// Task 7
+// document.write("<h1><b>Shopping Cart</b></h1>" +"<br>");
+// var item1=200;
+// var item2=300;
+// var charges=100;
+// document.write("The price of item1 is"+item1 +"<br>");
+// document.write("The quantity of item1 is 2"+"<br>");
+// document.write("The price of item2 is"+item2 +"<br>");
+// document.write("The quantity of item2 is 3"+"<br>");
+// document.write("The shipping charges is"+ charges +"<br>");
+// var total=(item1*2)+(item2*3)+charges;
+// document.write("Total cost is "+total);
+
+// Task 8
+// document.write("<h1>Mark Sheet</h1>"+"<br>");
+// var total=950;
+// var obtained=927;
+// var percentage = (obtained/total)*100;
+// document.write("Total marks is:"+total+"<br>");
+// document.write("Obtained marks is:"+obtained+"<br>");
+// document.write("The Result is "+percentage+"%");
+
+// Task 9
+// var dollar=10*165.4;
+// var reyal=28*25;
+// var total=dollar+reyal;
+// document.write("<h1>Pakistan Currency</h1>"+"<br>");
+// document.write("The total Pakistani Rupees is "+total+" PKR");
+
+// Task 10
+// var test_number=10;
+// var cal =test_number+5*10/2;
+// document.write("The expression after calculation is "+ cal);
+
+// Task 11
+// document.write("<h1>Age Calculator</h1>"+"<br>");
+// var current_year=2020;
+// var birth=2000;
+// var age = current_year-birth;
+// document.write("Current year is:"+current_year+"<br>");
+// document.write("Birth year is:"+birth+"<br>");
+// document.write("Your age is:"+age+"<br>");
+
+// Task 12
+// var radius=10;
+// var pi=3.142;
+// var circum=2*pi*radius;
+// var area = pi*radius*radius;
+// document.write("<h1>Geometry</h1>"+"<br>");
+// document.write("Radius is:"+radius+"<br>");
+// document.write("Circumference of circle is:"+circum+"<br>");
+// document.write("Area of circle is:"+area+"<br>");
+
+// Task 13
+// document.write("<h1>LifeTime Supplier Calculator</h1>"+"<br>");
+// var snak="Sooper";
+// var age=20;
+// var max =60;
+// var snak_per_day=2;
+// var calc= (max-age)*365*2;
+// document.write("Favourite Snake "+snak+"<br>");
+// document.write("Age "+age+"<br>");
+// document.write("Maximum Age "+max+"<br>");
+// document.write("Snak per days is "+snak_per_day+"<br>");
+// document.write("You would need to "+calc+" more snak at the end of the life");
