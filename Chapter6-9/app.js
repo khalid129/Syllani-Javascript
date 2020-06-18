a5874e80feddcb4985931f5d93e5610aaf12082b
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-19 01:18:55 +0500

    javascript Assignment Chapter 6-9

---
+++ Chapter6-9/app.js
@@ -0,0 +1,57 @@
+// CHAPTER 6-9
+// Task 1
+// var a = 10;
+// document.write("Result"+" <br>")
+// document.write("the value of a is "+a+" <br>"+" <br>");
+// document.write("the value of ++a is "+ ++a+" <br>");
+// document.write("Now the value of a is "+a+" <br>"+" <br>");
+// document.write("the value of a++ is "+ a++ +" <br>");
+// document.write("Now the value of a is "+a+" <br>"+" <br>");
+// document.write("the value of --a is "+ --a +" <br>");
+// document.write("Now the value of a is "+a+" <br>"+" <br>");
+// document.write("the value of a-- is "+ a-- +" <br>");
+// document.write("Now the value of a is "+a+" <br>");
+
+// Task 2
+// var a=2,b=1;
+// document.write("value of a is "+a+" <br>");
+// document.write("value of b is "+b+" <br>");
+// var result=--a - --b + ++b +b--;
+// document.write("Result is "+result+" <br>");
+
+// Task 3
+// var name = prompt("Enter your name");
+// alert("Your name is "+ name);
+
+// Task 5
+// var num=prompt("Enter a number for Table");
+// if (num==""){
+//     var table1 = 5;
+//     for(var i=1 ;i<=10;i++){
+//         document.write(table1+"x"+i+"="+table1*i+"<br>");
+//     }
+// }
+// else{
+//     var table=parseInt(num);
+// for(var i=1 ;i<=10;i++){
+//     document.write(table+"x"+i+"="+table*i+"<br>");
+// }
+// }
+
+// Task 6
+// var subject1 = prompt("Enter 1st Subject name");
+// var subject2 = prompt("Enter 2nd Subject name");
+// var subject3 = prompt("Enter 3rd Subject name");
+// var totalMarks = 100;
+// var sum = totalMarks*3;
+// var obtained1 = prompt("Enter obtained marks of "+ subject1);
+// var obtained2 = prompt("Enter obtained marks of "+ subject2);
+// var obtained3 = prompt("Enter obtained marks of "+ subject3);
+// var num1 = parseInt(obtained1);
+// var num2 = parseInt(obtained2);
+// var num3 = parseInt(obtained2);
+// var totalNum = num1+num2+num3;
+// var percentage1 = (num1*100)/100;
+// var percentage2 = (num2*100)/100;
+// var percentage3 = (num3*100)/100;
+// var totalPercentage = (percentage1+percentage2+percentage3)/3;
