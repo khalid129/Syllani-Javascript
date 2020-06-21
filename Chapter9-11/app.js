69727869cace08461928bff5e8908794897198ec
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-21 09:20:50 +0500

    Javascript Assignment Chapter9-11

--- Chapter9-11/app.js
+++ Chapter9-11/app.js
@@ -20,3 +20,28 @@
 // else{
 //     alert("Good Morning");
 // }
+
+// Task 3
+// var color=prompt("Enter color");
+// if (color=="red"){
+//     alert("Must stop");
+// }
+// else if(color=="yellow"){
+//     alert("Ready to move");
+// }
+// else if(color=="green"){
+//     alert("Move now");
+// }
+// else{
+//     alert("wait for light turn green");
+// }
+
+// Task 4
+// var fuel=prompt('Enter amount of fuel remaining in your car');
+// var a =parseFloat(fuel);
+// if (a<0.25){
+//     alert("please refuel your car");
+// }
+// else{
+//     alert('your fuel is quit enough');
+// }
