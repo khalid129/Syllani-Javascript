b8d43b0abf3cda9fdfef0c7f1c42cfd7e4896d22
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-06-21 09:19:57 +0500

    Javascript Assignment Chapter9-11

---
+++ Chapter9-11/app.js
@@ -0,0 +1,22 @@
+// CHAPTER 9-11
+// Task 1
+// var cities=["Karachi","Lahore","Islamabad","Faisalabad"];
+// var input=prompt("Enter your city");
+// for(var i=0;i<cities.length-1;i++){
+//     var check=cities[i]
+//     if(check=="Karachi"){
+//         alert("Welcome to City of light")
+//     }
+// }
+
+// Task 2
+// var gender=prompt("Enter your Gender");
+// if (gender=="male"||gender=="Male"){
+//     alert("Good Morning Sir");
+// }
+// else if(gender=="female"||gender=="Female"){
+//     alert("Good Morning Madam");
+// }
+// else{
+//     alert("Good Morning");
+// }
