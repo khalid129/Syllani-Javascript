362dfac0e5ad4f6ac133aa5ffcfdcf43be0ce287
Muhammad khalid <muhammadkhalid.ch129@gmail.com>
2020-07-18 11:54:29 +0500

    Javascript Assingmnet Chapter 12-13

---
+++ Chapter12-13/app.js
@@ -0,0 +1,81 @@
+// CHAPTER 12-13
+// Task 1
+// var numCheck = prompt("Enter a character");
+// if (numCheck>="a"&&numCheck<="z"){
+//     alert("you character "+numCheck+" is in lower caase");
+// }
+// else if(numCheck>="A"&&numCheck<="Z"){
+//     alert("you character "+numCheck+" is in upper caase");
+// }
+// else{
+//     alert("Your character is invalid");
+// }
+
+// Task 2
+// var integer1 = prompt("Enter 1st integer");
+// var integer2 = prompt("Enter 2nd integer");
+// var int1 = parseInt(integer1);
+// var int2 = parseInt(integer2);
+// if (int1>int2){
+//     alert(int1 + " is grater than "+ int2);
+// }
+// else if(int1<int2){
+//     alert(int2+" is greater than "+int1);
+// }
+// else{
+//     alert(int1 +" and "+int2+ " are equal" );
+// }
+
+// Task 3
+// var num = prompt("Enter any number");
+// var check = parseInt(num);
+// if(check>0){
+//     alert("The number is positive");
+// }
+// else if (check<0){
+//     alert("The number is negative");
+// }
+// else{
+//     alert("The number is Zero");
+// }
+
+// Task 4
+// var  text = prompt("Enter a character");
+// if (text == "A" || text == "E" || text == "I" || text == "O" || text == "U" || text == "a" || text == "e" || text == "i" || text == "o" || text == "u"){
+//     alert("True");
+// }
+// else{
+//     alert("False");
+// }
+
+// Task 5
+// var password = "khalid";
+// var checkPassword = prompt("Please Enter your password");
+// if (checkPassword === password){
+//     alert("Correct Password");
+// }
+// else{
+//     alert("Inncorrect Password!");
+// }
+
+// Task 6
+// var greeting;
+// var hour = 13;
+//  if (hour < 18) { greeting = "Good day";}
+// else{ greeting = "Good evening"; }
+
+// Task 7
+// var time = prompt("Enter time in this format 1900 = 7pm")
+// var timeConvert = parseInt(time);
+// if(timeConvert>=0 && timeConvert <1200){
+//     alert('Good Morning');
+// }
+// else if (timeConvert >=1200 && timeConvert <1700){
+//     alert("Good After noon");
+// }
+// else if (timeConvert>=1700 && timeConvert<2100){
+//     alert("Good Evening");
+// }
+// else if(timeConvert>=2100 && timeConvert<=2359){
+//     alert("Good Night");
+// }
